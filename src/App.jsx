58271317f1bb9cd4866b72e44a3c5e234fa4b2c1
zhao-18 <45818093+zhao-18@users.noreply.github.com>
2025-02-26 17:48:41 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -177,8 +177,8 @@ const Center = ({ token, onNewLocalization, onFileUpload }) => {
                 openText="Create New ▾"
                 closedClassName="border-solid border-1 border-sky-700 text-sky-700 font-semibold rounded-xl w-64 h-12 mt-5"
                 openClassName="bg-sky-700 text-white font-semibold rounded-xl w-64 h-12 mt-5"
-                options="Localization Stimulation"
-                optionRefs="/localization /stimulation"
+                options="Localization Stimulation Debug"
+                optionRefs="/localization /stimulation /debug"
                 optionClassName="block w-64 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 menuClassName="w-64 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                 onOptionClick={(option) => {
